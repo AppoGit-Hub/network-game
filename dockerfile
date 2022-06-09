e3dd1966e89223088efd2830cce720fb642a0653
AppoGit-Hub <bl@dtd.be>
2022-06-09 18:26:50 +0200

    python 3.9.2 support + docker

--- dockerfile
+++ dockerfile
@@ -1,4 +1,9 @@
 FROM python:3
+FROM ubuntu
+
+RUN set -xe \
+    && apt-get update -y \
+    && apt-get install -y python3-pip
 
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
--- server.py
+++ server.py
@@ -25,41 +25,37 @@ while True:
     data, addr = server.recvfrom(constant.PACKET_SIZE)
     client_packet : ClientPacket = pickle.loads(data)
     
-    match client_packet.code:
-        case code.CONNECT:
-            print(f"Connect! {addr}")
-            clients.append(addr)  
-            server.sendto(pickle.dumps(ServerPacket(client_packet.id, True, None)), addr)
+    if client_packet.code == code.CONNECT:
+        print(f"Connect! {addr}")
+        clients.append(addr)  
+        server.sendto(pickle.dumps(ServerPacket(client_packet.id, True, None)), addr)
+    elif client_packet.code == code.DISCONNECT:
+        print(f"Disconnect! {addr}")
+        clients.remove(addr)
+        server.sendto(pickle.dumps(ServerPacket(client_packet.id, True, None)), addr)
+    elif client_packet.code == code.POSITION_CHANGED:
+        rectangle : Rectangle = client_packet.data
+        server_packet = ServerPacket(client_packet.id, True, Vector(rectangle.x, rectangle.y))
+        x_correct = 0 <= rectangle.x and rectangle.x <= (constant.RESOLUTION_WIDTH - rectangle.width)
+        y_correct = 0 <= rectangle.y and rectangle.y <= (constant.RESOLUTION_HEIGHT - rectangle.height)
 
-        case code.DISCONNECT:
-            print(f"Disconnect! {addr}")
-            clients.remove(addr)
-            server.sendto(pickle.dumps(ServerPacket(client_packet.id, True, None)), addr)
+        if x_correct and y_correct:
+            print(f"Position changed: {rectangle}, on {addr}")
+            pass
+        else:
+            correct_x = rectangle.x
+            correct_y = rectangle.y
+            if not x_correct:
+                correct_x = clamp(rectangle.x, 0, constant.RESOLUTION_WIDTH - rectangle.width)
+            if not y_correct:
+                correct_y = clamp(rectangle.y, 0, constant.RESOLUTION_HEIGHT - rectangle.height)
 
-        case code.POSITION_CHANGED:
-            rectangle : Rectangle = client_packet.data
-            server_packet = ServerPacket(client_packet.id, True, Vector(rectangle.x, rectangle.y))
-            x_correct = 0 <= rectangle.x and rectangle.x <= (constant.RESOLUTION_WIDTH - rectangle.width)
-            y_correct = 0 <= rectangle.y and rectangle.y <= (constant.RESOLUTION_HEIGHT - rectangle.height)
-
-            if x_correct and y_correct:
-                print(f"Position changed: {rectangle}, on {addr}")
-                pass
-            else:
-                correct_x = rectangle.x
-                correct_y = rectangle.y
-                if not x_correct:
-                    correct_x = clamp(rectangle.x, 0, constant.RESOLUTION_WIDTH - rectangle.width)
-                if not y_correct:
-                    correct_y = clamp(rectangle.y, 0, constant.RESOLUTION_HEIGHT - rectangle.height)
-
-                server_packet.validation = False
-                server_packet.correction = Vector(correct_x, correct_y)
-                print(f"Position cant be changed {rectangle}, on {addr}")
-            server.sendto(pickle.dumps(server_packet), addr)
-
-        case _:
-            print("Did not find")
-            break
+            server_packet.validation = False
+            server_packet.correction = Vector(correct_x, correct_y)
+            print(f"Position cant be changed {rectangle}, on {addr}")
+        server.sendto(pickle.dumps(server_packet), addr)
+    else:
+        print("Did not find")
+        break
 
 server.close()
